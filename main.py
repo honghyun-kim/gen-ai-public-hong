a70433a8cf7dba08849a465dde3889befd356d2b
honghyun-kim <81349637+honghyun-kim@users.noreply.github.com>
2024-11-15 15:15:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-st.title('최유나 이거 보임??? ')
-st.title('This 한글도 됨? 수정')
+st.title('안녕하세요. 테스트 하고 있습니다. by khh')
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
-
 
 
 title = st.text_input("주제를 정해봐")
@@ -12,3 +10,7 @@ st.write("The current movie title is", title)
 
 if st.button("요청하기"):
     st.write("뭐임마")
+
+
+if st.button("이것도 눌러봐"):
+    st.write("ㅄ...")
